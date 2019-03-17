2913b627d673717ce78e3622081249692420dedf
zhizhonghua <zhizhonghua@boco.com.cn>
2019-03-18 00:06:04 +0800

    版本初始化

---
+++ src/main/java/net/cc/demo_springboot_aop/DemoSpringbootAopApplication.java
@@ -0,0 +1,13 @@
+package net.cc.demo_springboot_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootAopApplication.class, args);
+    }
+
+}
